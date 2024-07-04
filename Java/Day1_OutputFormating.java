2494b0162b00c9dc42fa223add0ecc48b76b51d9
SurajBadchikar <sierrabravo93441@gmail.com>
2024-07-04 21:17:08 +0530

    Day1_OutputFormating

--- Java/Day1_OutputFormating.java
+++ Java/Day1_OutputFormating.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OutputFormating {
+public class Day1_OutputFormating {
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
