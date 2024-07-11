95c86a36c62d7db6a05f67bc3e5ae773fce5b896
SurajBadchikar <sierrabravo93441@gmail.com>
2024-07-11 20:58:10 +0530

    Day1_Codes

---
+++ Projects/electricity/billing/system/Login.java
@@ -0,0 +1,14 @@
+package electricity.billing.system;
+
+import javax.swing.JFrame;
+
+public class Login extends JFrame {
+    Login() {
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
