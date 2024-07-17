e9e5c1e1a8fa2d663488ea0e45c52293defdd218
SurajBadchikar <sierrabravo93441@gmail.com>
2024-07-17 07:47:19 +0530

    Day6_Codes

---
+++ Day6_TwoSum.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Day6_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }    
+}
