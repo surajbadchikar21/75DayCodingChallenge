9cc16af7499ed97966a6da014800c3811c07c06f
SurajBadchikar <sierrabravo93441@gmail.com>
2024-08-04 13:35:07 +0530

    ..

---
+++ DAY13HackerrankSubArray.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DAY13HackerrankSubArray {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array1= new int[n];
+        for(int i=0;i<n;i++){
+            array1[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int j=0;j<n;j++){
+            int sum=0;
+            for(int k=j;k<n;k++){
+                sum=array1[k]+sum;
+                if(sum<0){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
