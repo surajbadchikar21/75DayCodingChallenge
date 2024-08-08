31f1464fd2161832ccfe4717665d37b2d9666e66
Suraj Badchikar <138509043+surajbadchikar21@users.noreply.github.com>
2024-08-08 23:03:03 +0530

    Updating Code

---
+++ Linked list/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        SinglyLinkedList l1 = new SinglyLinkedList();
+        l1.createSinglyLinkedList(10);
+        System.out.println(l1.head.value);
+    }
+}
--- Linked list/SinglyLinkedList.java
+++ Linked list/SinglyLinkedList.java
@@ -46,4 +46,4 @@ public class SinglyLinkedList {
 size+=1;
         }
     }
-}
+
